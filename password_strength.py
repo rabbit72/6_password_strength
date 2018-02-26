8249854dad441344b1c61f7bdf134481774e060e
Daniil Shadrin <rabbit72rus@gmail.com>
2018-02-26 23:58:49 +0300

    fill template

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,42 @@
+import re
+import sys
+import json
+
+PATH_TO_BLACKLIST = 'blacklist_passwords'
+
+
+def load_blacklist(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file_blacklist:
+        return json.load(file_blacklist)
 
 
 def get_password_strength(password):
-    pass
+    mark_password = 1
+    black_list = load_blacklist(PATH_TO_BLACKLIST)
+    if password in black_list or len(password) <= 6:
+        return mark_password
+    mark_password += 1 if len(password) >= 8 else 0
+    mark_password += 1 if len(password) >= 10 else 0
+    mark_password += 1 if len(password) >= 12 else 0
+    mark_password += 1 if len(password) >= 14 else 0
+    mark_password += 1 if len(password) >= 16 else 0
+    mark_password += 1 if re.search('[0-9]', password) else 0
+    mark_password += 1 if re.search('[a-z]', password) else 0
+    mark_password += 1 if re.search('[A-Z]', password) else 0
+    pattern = '[.,<>+=*!@#$%^&/|`~№?"\'(){}\[\]_«»—:;]'
+    mark_password += 2 if re.search(pattern, password) else 0
+    return mark_password
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        while True:
+            password = input('Enter password for evaluation: ')
+            if password:
+                break
+        password_strength = get_password_strength(password)
+        print('Password strength: {0}/10 '.format(password_strength))
+    except json.JSONDecodeError:
+        sys.exit('Blacklist passwords are not JSON')
+    except FileNotFoundError:
+        sys.exit('Blacklist was not found. Check file: {0}'.format(PATH_TO_BLACKLIST))
