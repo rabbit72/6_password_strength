05a9f96eb1accc0b497f2f42cfaa03b0a0b280be
Daniil Shadrin <rabbit72rus@gmail.com>
2018-02-27 17:12:30 +0300

    remove constant, fix pattern

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,7 @@
 import re
 import sys
 import json
-
-PATH_TO_BLACKLIST = 'blacklist_passwords'
+from string import punctuation
 
 
 def load_blacklist(file_path):
@@ -12,7 +11,8 @@ def load_blacklist(file_path):
 
 def get_password_strength(password):
     mark_password = 1
-    black_list = load_blacklist(PATH_TO_BLACKLIST)
+    path_to_blacklist = 'blacklist_passwords'
+    black_list = load_blacklist(path_to_blacklist)
     if password in black_list or len(password) <= 6:
         return mark_password
     mark_password += 1 if len(password) >= 8 else 0
@@ -23,8 +23,7 @@ def get_password_strength(password):
     mark_password += 1 if re.search('[0-9]', password) else 0
     mark_password += 1 if re.search('[a-z]', password) else 0
     mark_password += 1 if re.search('[A-Z]', password) else 0
-    pattern = '[.,<>+=*!@#$%^&/|`~№?"\'(){}\[\]_«»—:;]'
-    mark_password += 2 if re.search(pattern, password) else 0
+    mark_password += 2 if re.search(punctuation, password) else 0
     return mark_password
 
 
