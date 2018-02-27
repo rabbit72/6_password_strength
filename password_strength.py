076d367bfdd6c0adbf3b5c891881baeb8e83f182
Daniil Shadrin <rabbit72rus@gmail.com>
2018-02-27 18:12:17 +0300

    fix search symbols

--- password_strength.py
+++ password_strength.py
@@ -13,7 +13,7 @@ def get_password_strength(password):
     mark_password = 1
     path_to_blacklist = 'blacklist_passwords'
     black_list = load_blacklist(path_to_blacklist)
-    if password in black_list or len(password) <= 6:
+    if password in black_list or len(password) < 6:
         return mark_password
     mark_password += 1 if len(password) >= 8 else 0
     mark_password += 1 if len(password) >= 10 else 0
@@ -23,7 +23,7 @@ def get_password_strength(password):
     mark_password += 1 if re.search('[0-9]', password) else 0
     mark_password += 1 if re.search('[a-z]', password) else 0
     mark_password += 1 if re.search('[A-Z]', password) else 0
-    mark_password += 2 if re.search(punctuation, password) else 0
+    mark_password += 2 if re.search('[{0}]'.format(punctuation), password) else 0
     return mark_password
 
 
